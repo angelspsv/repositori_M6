c3348f29bf871b94083ef5d217e6b19baa147386
angel_ivanov <angelspsv@gmail.com>
2025-01-22 12:56:32 +0100

    boto return cap a index funcionant

--- script/playlist.js
+++ script/playlist.js
@@ -1,11 +1,14 @@
 const accessToken = window.location.href.split("access_token=")[1];
 
+const API_URL_SEVERAL_TRACKS = "https://api.spotify.com/v1/tracks";
+
 //A la pàgina de la playlist defi nir una variable global per guardar el token
 let token = "";
 
 
 //funcio per retornar el usuari a la pagina de inici
 function tornarInici(){
+    window.location.href = "index.html";
     console.log('Tornem enrere nomes de prova');
 }
 
@@ -20,3 +23,61 @@ btnReturn.classList.add('button-style');
 btnReturn.addEventListener('click', tornarInici);
 //afegim el boto al espai html creat per aquest
 document.getElementById('tornar').appendChild(btnReturn);
+
+
+
+const renderTrackSelected = function(tracks){
+
+}
+
+
+//retorna la llista de les cançons desades per el usuari
+const getPlayList = function(){
+
+}
+
+const getIdTracksLocalStorage = function(){
+    return localStorage.getItem('trackList');
+}
+
+const getTrack = async function(llistaTracks){
+    const urlEndpoint = `${API_URL_SEVERAL_TRACKS}?ids=${llistaTracks}`;
+    try{
+        //cridar la api
+        const resposta = await fetch(urlEndpoint, {
+            "method": GET,
+            "headers": {
+                Authorization: `Bearer ${accessToken}`,
+            }
+        });
+
+        if(!resposta.ok){
+            throw Error("Error al fer la consulta", resposta.status);
+        }
+
+        //consultar la informacio
+        const tracks = await resposta.json();
+
+        renderTrackSelected();
+    }
+
+    catch (error){
+        console.log(error);
+    }
+}
+
+
+// retorna les cançons desades al local_storage
+const getTrackSelected = function(){
+    let llistaTracks = getIdTracksLocalStorage();
+    llistaTracks = llistaTracks.replaceAll(";", ",");
+    getTrackSelected();
+    //get track localstorage
+    //cridar el endpoint https api.spotify.com/v1/tracs
+
+}
+
+
+//com son funcions anonimes primer s'han de definir i despres cridar-les
+getPlayList();
+getTrackSelected();
